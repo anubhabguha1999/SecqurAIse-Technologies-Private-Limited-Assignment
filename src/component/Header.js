65140479040765a919564e1a8b12dd129623d233
Anubhab Guha <anubhabguha1999@gmail.com>
2023-04-04 17:14:33 +0530

    first commit

---
+++ src/component/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <>
+    <div className="h-15 text-left text-[black] border border-[black] text-5xl font-serif pl-10 font-weight: bold">Design</div>
+    </>
+  )
+}
+
+export default Header
